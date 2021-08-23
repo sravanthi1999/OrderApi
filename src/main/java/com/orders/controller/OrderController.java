803e9ed951f579d67a0063539a898ac19824667d
Sravanthi <shivannagarisravanthi@gmail.com>
2021-08-23 18:28:55 +0530

    filters

--- src/main/java/com/orders/controller/OrderController.java
+++ src/main/java/com/orders/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.orders.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orders.model.Order;
-import com.orders.repo.OrderRepo;
 import com.orders.service.OrderService;
 
 //import antlr.collections.List;
@@ -22,57 +22,67 @@ import com.orders.service.OrderService;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
-	
+
 	@Autowired
 	OrderService orderService;
-	
-	@Autowired
-	OrderRepo orderRepo;
-	
+
+//	@Autowired
+//	OrderRepo orderRepo;
+
 	@GetMapping("/allorders")
 	public List<Order> getOrders()
 	{
 		return orderService.allOrders();
-		
+
 	}
-	
+
 	@PostMapping("/saveorder")
 	public void saveOrder(@RequestBody Order order)
 	{
 		orderService.saveOrder(order);
 	}
-
+	
+	//custom query get by orderId
 	@GetMapping("/orderid")
 	@ResponseBody
 	public List<Order> getbyOrderId(@RequestParam int orderId) {
 		return orderService.getbyOrderId(orderId);
 	}
-	
+
+	//custom query get by orderId and fr Id
 	@GetMapping("/ids")
 	@ResponseBody
 	public Order getbyOrderIdAndFrId(@RequestParam int orderId, @RequestParam String frId)
 	{
 		return orderService.getbyOrderIdAndFrId(orderId, frId);
 	}
+
+	//	@GetMapping("/orderspage")
+	//	Page<Order> paginate(Pageable pageable)
+	//	{
+	//		return orderRepo.findAll(pageable);
+	//	}
+
+
 	
+	//getting paginated all data without filtering
+
+	@GetMapping("/orderspage")
+	@ResponseBody
+	public Page<Order> paginate(@RequestParam int pageNum,@RequestParam int pageSize){
+
+		return orderService.paginate(pageNum,pageSize);
+	}
+
+
+
+	//custom query  pagination based on filtering orderID
+
 	@GetMapping("/paginate/ids")
 	@ResponseBody
-	public List<Order> paginateOrderIdAndFrId(@RequestParam int orderId, @RequestParam int pageNum,@RequestParam int pageSize)
+	public Page<Order> paginateOrderIdAndFrId(@RequestParam int orderId, @RequestParam int pageNum,@RequestParam int pageSize)
 	{
 		return orderService.paginateOrderId(orderId,pageNum,pageSize);
 	}
-	
-	@GetMapping("/orderspage")
-	@ResponseBody
-	public List<Order> paginate(@RequestParam int pageNum,@RequestParam int pageSize){
-		
-		return orderService.paginate(pageNum,pageSize);
-	}
-	
-//	@GetMapping("/orderspage")
-//	Page<Order> paginate(Pageable pageable)
-//	{
-//		return orderRepo.findAll(pageable);
-//	}
-	
+
 }
--- src/main/java/com/orders/service/OrderService.java
+++ src/main/java/com/orders/service/OrderService.java
@@ -17,40 +17,87 @@ public class OrderService {
 
 	@Autowired
 	OrderRepo orderRepo;
-	
+
 	public List<Order> allOrders() {
-		
+
 		return orderRepo.findAll();
 	}
-	
+
 	public void saveOrder(Order order) {
 		orderRepo.save(order);
 	}
-	
+
+	//custom query get by orderId
 	public List<Order> getbyOrderId(int orderId)
 	{
 		return orderRepo.OrderId(orderId);
 	}
-	
+
+	//custom query get by order and fr ID's
 	public Order getbyOrderIdAndFrId(int orderId,String frId)
 	{
 		return orderRepo.OrderIdFrId(orderId, frId);
 	}
+	
+	
 
-	public List<Order> paginate(int pageNum, int pageSize) {
-		
+	//getting paginated data without filtering
+
+	public Page<Order> paginate(int pageNum, int pageSize) {
+
 		Pageable pageable=PageRequest.of(pageNum, pageSize);
 		Page<Order> resPage=orderRepo.findAll(pageable);
-		
-		return resPage.getContent();
+
+		return resPage;
 	}
 
-	public List<Order> paginateOrderId(int orderId,int pageNum, int pageSize) {
-		
+
+	//custom query  pagination based on filtering orderID
+
+	public Page<Order> paginateOrderId(int orderId,int pageNum, int pageSize) {
+
 		Pageable pageable=PageRequest.of(pageNum, pageSize);
 		Page<Order> resPage=orderRepo.pagingById(orderId,pageable);
-		
-		return resPage.getContent();		
+
+		return resPage;		
 	}
-	
+
 }
+
+
+
+//
+//{
+//   
+//    "pageable": 
+//    {
+//        "sort": 
+//        {
+//            "sorted": false,
+//            "unsorted": true,
+//            "empty": true
+//        },
+//        "offset": 1,
+//        "pageNumber": 0,
+//        "pageSize": 1,
+//        "paged": true,
+//        "unpaged": false
+//    },
+//    "last": true,
+//    "totalElements": 2,
+//    "totalPages": 2,
+//    "size": 1,
+//    "number": 1,
+//    "sort": 
+//    {
+//        "sorted": false,
+//        "unsorted": true,
+//        "empty": true
+//    },
+//    "first": false,
+//    "numberOfElements": 1,
+//    "empty": false
+//}
+
+
+
